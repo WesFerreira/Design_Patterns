dd059bd8b1ed09e6caaa2ec8b50357035113a3e4
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-04-23 13:46:22 -0300

    initial commit

---
+++ src/main/java/br/com/wesley/loja/orcamento/Orcamento.java
@@ -0,0 +1,16 @@
+package br.com.wesley.loja.orcamento;
+
+import java.math.BigDecimal;
+
+public class Orcamento {
+
+    private BigDecimal valor;
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
