54500a1a09c763853b0f9675fc973576703b6ba9
Wesley Ferreira da Silva <ferreiras.wesley@gmail.com>
2023-04-24 09:29:53 -0300

    apply the Design Pattern Strategy

--- src/main/java/br/com/wesley/loja/TesteImpostos.java
+++ src/main/java/br/com/wesley/loja/TesteImpostos.java
@@ -1,7 +1,8 @@
 package br.com.wesley.loja;
 
 import br.com.wesley.loja.imposto.CalculadoraImpostos;
-import br.com.wesley.loja.imposto.TipoImposto;
+import br.com.wesley.loja.imposto.ICMS;
+import br.com.wesley.loja.imposto.ISS;
 import br.com.wesley.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -11,8 +12,7 @@ public class TesteImpostos {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100.0"));
         CalculadoraImpostos calculadora = new CalculadoraImpostos();
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ICMq
-                S));
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
     }
 
 }
